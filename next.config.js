e1e5c922cd98ea7995208929e36ae87a8e601df0
Alexander Ulbrich <adulbrich@users.noreply.github.com>
2022-03-29 08:47:51 -0700

    added security policies

--- next.config.js
+++ next.config.js
@@ -1,3 +1,50 @@
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' plausible.io;
+`;
+
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "strict-origin-when-cross-origin",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=()",
+  },
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+  },
+  // Strict-Transport-Security not required as we deploy on Vercel and Vercel does this automatically
+];
+
 module.exports = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
