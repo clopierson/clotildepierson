b2ceb87130673fdbcbd77e0e6fe7ba4b3e4a6ed0
Alexander Ulbrich <adulbrich@users.noreply.github.com>
2022-05-05 10:21:32 -0700

    updated position description

--- components/phdPositions2022.js
+++ components/phdPositions2022.js
@@ -3,9 +3,8 @@ export default function Recruiting() {
     <article className="rounded bg-neutral-100 dark:bg-neutral-800 p-3 sm:p-6 shadow-inner dark:shadow-black/50">
       <h2 className="mt-0 text-lg">Open Ph.D. Position</h2>
       <p>
-        We have one (1) open Ph.D. position for Fall 2022. The Ph.D. student
-        will be working on the following project (click the link to open the
-        position description):{" "}
+        We have one (1) open Ph.D. position for Fall 2022. Click the link for
+        more information about the position and how to apply:{" "}
         <a
           href="/files/PositionDescription_UrbanVisualEnvironment.pdf"
           alt="phd position description pdf file"
@@ -13,18 +12,6 @@ export default function Recruiting() {
           rel="noopener noreferrer"
         >
           Present and future indoor light and visual conditions in urban areas
-        </a>
-        .
-      </p>
-      <p className="mb-0">
-        Please submit your application by{" "}
-        <a
-          href="https://airtable.com/shrnQFJihLL8YU878"
-          alt="2022 RadiantLab PhD application form"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          completing the form
         </a>
         .
       </p>
