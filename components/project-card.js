b6dd6debed9c1167dd590b93adc460a9150d3abf
Alexander Ulbrich <adulbrich@users.noreply.github.com>
2026-06-05 20:39:23 -0700

    fix: project card internal margins

--- components/project-card.js
+++ components/project-card.js
@@ -1,9 +1,9 @@
 export default function ProjectCard({ projectName, projectDescription }) {
   return (
     <article className="rounded bg-neutral-100 p-3 shadow-inner sm:p-6 dark:bg-neutral-800 dark:shadow-black/50">
-      <p className="my-0 text-xs uppercase">selected project</p>
-      <h2 className="my-2 text-base">{projectName}</h2>
-      <p className="my-0">{projectDescription}</p>
+      <p className="mt-0 mb-2 text-xs uppercase">selected project</p>
+      <h2 className="mt-2 text-base">{projectName}</h2>
+      <p className="my-0 mb-0">{projectDescription}</p>
     </article>
   );
 }
